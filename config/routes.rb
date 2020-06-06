e4769bd723c2da89660d15a0f18fe6c3c6bc7148
Ikko-T <skyblueboy32@gmail.com>
2020-06-06 16:45:43 +0900

    added user info

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
 
-  get "/" => "home#top"
+  get '/users/index', to: 'users#index'
+  
+  get '/posts/new', to: 'posts#new'
+  
+  get '/posts/index', to: 'posts#index'
+
+  get '/posts/:id', to: 'posts#show'
+
+  post '/posts/create', to: 'posts#create'
+
+  get '/posts/:id/edit', to: 'posts#edit'
+
+  post '/posts/:id/update', to: 'posts#update'
+
+  post '/posts/:id/destroy', to: 'posts#destroy'
+
+  get '/top', to: 'home#top'
+
+  get '/about', to: 'home#about'
+
 end
