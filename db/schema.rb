220df77de763a1b4f5a52e77756042e70ffb8d08
Ikko-T <skyblueboy32@gmail.com>
2020-06-07 14:53:17 +0900

    added the image_uploading feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_062457) do
+ActiveRecord::Schema.define(version: 2020_06_07_000007) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_06_06_062457) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_name"
   end
 
 end
