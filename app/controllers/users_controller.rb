3e23d844c9f8fab3503a3c24febfa9678d1481da
Ikko-T <skyblueboy32@gmail.com>
2020-06-08 20:28:51 +0900

    added the login/out feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
   
   def index
     @users = User.all
@@ -16,9 +18,11 @@ class UsersController < ApplicationController
     @user = User.new(
       name: params[:name],
       email: params[:email]),
-      image_name: "default_user.png"
+      image_name: "default_user.png",
+      password: params[:password]
     )
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -46,4 +50,26 @@ class UsersController < ApplicationController
       render("users/edit")
     end
   end
+
+  def login_form
+  end
+
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else
+      @error_message ="メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
+
+    def logout
+      session[:user_id] = nil
+      flash[:notice] = "ログアウトしました"
+      redirect_to("/login")
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_000007) do
+ActiveRecord::Schema.define(version: 2020_06_08_044933) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_000007) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_name"
+    t.string "password"
   end
 
 end
