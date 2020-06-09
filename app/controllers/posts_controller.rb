3f9b3b4c13b77d91074ad6fea9be95f8b4951010
Ikko-T <skyblueboy32@gmail.com>
2020-06-09 11:09:47 +0900

    added connections User and Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
     before_action :authenticate_user
-    
+    before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
+
     # 投稿データを取得するアクション
     def index
         @posts = Post.all.order(created_at: :desc)
@@ -9,6 +10,7 @@ class PostsController < ApplicationController
     # 投稿データを表示するアクション
     def show
         @post = Post.find_by(id: params[:id])
+        @user = @post.user
     end
 
     # 新規習慣化を作成するアクション
@@ -18,7 +20,7 @@ class PostsController < ApplicationController
 
     # 新規作成した習慣リストを送信するアクション
     def create
-        @post = Post.new(content: params[:content])
+        @post = Post.new(content: params[:content], user_id: @current_user.id)
         if @post.save
             flash[:notice] = "新規作成しました"
             redirect_to("/posts/index") #指定のURLに転送する
@@ -49,5 +51,13 @@ class PostsController < ApplicationController
         @post.destroy
         flash[:notice] = "削除しました"
         redirect_to("/posts/index")
+    end
+
+    def ensure_correct_user
+        @post = Post.find_by(id: params[:id])
+        if @post.user_id != @current_user.id
+            flash[:notice] = "権限がありません"
+            redirect_to("/posts/index")
+        end
     end
 end
